5348eede0a6c7e16dfd739341a08ba71fd7f1d04
FRickReich <frickreich@gmail.com>
2021-08-31 09:40:39 +0200

    Add comment examples

---
+++ 3_PB/1_language/0_comments.js
@@ -0,0 +1,44 @@
+// Einzeilige kommentare
+
+// Dies ist eine zeile
+// Nächste zeile
+// Noch eine zeile...
+
+// Ein einzeiliges kommentar, geht immer nur über eine zeile und wird mit zwei schrägstrichen eingeleitet.
+
+// bla sollte man umbennen.
+const bla = 5;
+
+/* Mehrzeilige kommentare */
+
+/*
+    Dies ist ein
+    mehrzeiliger kommentar
+
+    wir können ihn so lang machen wie wir wollen...
+*/
+
+// Ein mehrzeiliger kommentar wird mit schrägstrich und sternchen eingeleitet und in der gleichen, oder einer folgenden zeile mit sternchen schrägstrich wieder geschlossen.
+
+// JSDoc style kommentare
+
+// JSdoc ist eine npm package, die interaktive und dynamische kommentare zulässt. sie wird per npm install -g jsdoc installiert.
+
+/**
+ * Dies ist ein JSDoc
+ * kommentar...
+ */
+
+// JSDoc in action
+
+/**
+ * @function testFunktion
+ * @see http://google.de
+ * @description Dies ist eine testfunktion
+ * @param { string } testWert1 ist ein testparameter
+ * @returns { string }
+ */
+function testFunktion(testWert1)
+{
+    
+}
