2132635c05de878d2123599a6ddfeace1cf99e3f
FRickReich <frickreich@gmail.com>
2021-09-01 10:08:48 +0200

    Add divider example

---
+++ 3_PB/1_language/divider.js
@@ -0,0 +1,23 @@
+
+// Simple Funktion um einen divider im terminal anzuzeigen.
+function divider(input)
+{
+    console.log("\n" + "=".repeat(20), input, "=".repeat(20), "\n");
+}
+
+// Divider ohne funktion:
+const spacer = "=".repeat(20);
+console.log(spacer, "Aufgabe 1", spacer, "\n");
+
+divider("Aufgabe 1");
+
+console.log("ergebnis");
+console.log(5)
+
+divider("Aufgabe 2");
+
+console.log("ABC");
+
+divider("Aufgabe 3");
+
+console.log(45);
