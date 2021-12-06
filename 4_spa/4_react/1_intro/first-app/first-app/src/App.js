56c31738eec911121c235786ecc82567d2b76132
FRickReich <frickreich@gmail.com>
2021-12-06 11:47:30 +0100

    Add react example

---
+++ 4_spa/4_react/1_intro/first-app/first-app/src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+
+const isLoggedIn = false;
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+
+        { 
+          // Wichtig ist, das wir code, in JSX in brackets schreiben.
+          // If/Else können wir hier nur als ternary nutzen
+          isLoggedIn ?
+          <p>
+            Wir sind Eingeloggt
+          </p>
+          :
+          <p>
+            Wir sind nicht Eingeloggt
+          </p>
+        }
+
+        {
+          isLoggedIn && 
+          <i>Hallo Welt</i>
+        }
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
