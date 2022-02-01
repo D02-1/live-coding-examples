4e9b58127a4b9086a57f4f8bb220ec85909c8878
FRickReich <frickreich@gmail.com>
2022-02-01 16:15:46 +0100

    Add exercises

---
+++ 5_ba/2_server/X_uebungen/mini-server/README.md
@@ -0,0 +1,16 @@
+# :cartwheeling: Exercise
+
+## Time to write your first Node mini-server :rocket:
+
+1. the server should give back the string "Hello World"
+
+2. add the endpoint "./now"
+when the url "http://localhost:4000/now" is called, the current Date and Time should be displayed
+
+3. add the endpoint "./count"
+when the url "http://localhost:4000/count" is called, a number should be displayed, the number displayed should be count up everytime 
+
+
+## :weight_lifting: Bonus
+
+create a .env Datei, which holds the port-number you are listening to
---
+++ 5_ba/2_server/X_uebungen/mini-server/src/index.js
@@ -0,0 +1,55 @@
+require("dotenv").config();
+
+const http = require("http");
+const fs = require("fs");
+const moment = require("moment");
+
+const port = process.env.PORT;
+
+let counter = 0;
+
+const server = http.createServer((req, res) =>
+{
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+    switch (req.url)
+    {
+        case "/now":
+            res.write(moment().format('MMMM Do YYYY, h:mm:ss'));
+            res.end();
+            break;
+        case "/count":
+            counter++;
+
+            res.write(`Der aktuelle count ist: ${ counter }`);
+            res.end();
+            break;
+        case "/more":
+            fs.readFile("./more.txt", "utf-8", (err, content) =>
+            {
+                if(err)
+                {
+                    res.writeHead(404);
+                    res.write("ERROR: Datei nicht gefunden!");
+                }
+                else
+                {
+                    res.write(content);
+                }
+                
+                res.end();
+            });
+
+            break;
+        default:
+            res.writeHead(404);
+            res.write("ERROR 404 - Hier ist nix!");
+            res.end();
+            break;
+    }
+});
+
+server.listen(port, () =>
+{
+    console.log(`Der server hört zu auf ${ port }!`);
+});
