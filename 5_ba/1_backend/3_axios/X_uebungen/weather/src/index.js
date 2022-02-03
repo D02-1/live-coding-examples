1b76c15e12eb2f032363dead14963a3dc1440bcd
FRickReich <frickreich@gmail.com>
2022-02-03 16:05:02 +0100

    Add files

--- 5_ba/1_backend/3_axios/X_uebungen/weather/src/index.js
+++ 5_ba/1_backend/3_axios/X_uebungen/weather/src/index.js
@@ -9,14 +9,33 @@ const key = process.env.API_KEY;
 
 const api = axios.create({ baseURL: "https://api.openweathermap.org/data/2.5" });
 
-const formatCity = (data) =>
+const formatCity = data =>
 {
     return data.name + ", " + data.sys.country;
 };
 
-const formatTemperature = (data) =>
+const formatTemperature = data =>
 {
     return `${ data.main.temp.toFixed() }${ unit === "metric" ?  "°C" : "°F" }`
+}
+
+const formatForecast = data =>
+{
+    const forecastData = data.data.list;
+
+    const forecastArr = forecastData.map((forecast, i) =>
+    {
+        let date = new Date();
+        date.setTime(forecast.dt * 1000);
+
+        return {
+            day: date.getDay(),
+            hour: date.getHours(),
+            temperature: formatTemperature(forecast)
+        };
+    });
+
+    return forecastArr;
 }
 
 const app = async () =>
@@ -33,12 +52,26 @@ const app = async () =>
             }
         });
 
+        const res2 = await api.get("/forecast",
+        {
+            params:
+            {
+                q: city,
+                appid: key,
+                units: unit
+            }
+        });
+
         const data = res.data;
 
         console.log(
             `Is is now ${ formatTemperature(data) } in ${ formatCity(data) }\n` +
-            `The current weather conditions are: ${ data.weather[0].description }`
+            `The current weather conditions are: ${ data.weather[0].description }\n\n` + 
+            "Forecast:"
         );
+        
+        console.log(formatForecast(res2));
+        
     }
     catch (err)
     {
