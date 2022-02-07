c0ff41f5cddee035e41d6c9a5ff92bd9a210dd2e
FRickReich <frickreich@gmail.com>
2022-02-07 10:55:22 +0100

    Add express example 404

--- 5_ba/2_server/2_express/1_express-intro/express/src/index.js
+++ 5_ba/2_server/2_express/1_express-intro/express/src/index.js
@@ -1,6 +1,9 @@
 // Wir importieren das modul express:
 const express = require('express');
 
+// Wir importieren path, um mit pfaden auf dem system arbeiten zu können;
+const path = require('path');
+
 // Wir setzeen eine express instanz auf eine variable, in standardkonvention heisst diese 'app'
 const app = express();
 
@@ -19,6 +22,12 @@ app.get("/", (req, res) =>
     res.status(200).send("Hallo Express!");
 });
 
+app.get('/seite', (req, res) =>
+{
+    // Mit .sendFile(); können wir dateien angeben, die wir senden wollen:
+    res.status(200).sendFile(path.join(__dirname, '../public/index.html'));
+});
+
 // BEISPIEL POST:
 
 // um express zu sagen, das wir mit json arbeiten wollen, rufen wir express mit der methode .use() sie soll eine middleware verwenden, die per .json(); json umwandeln kann.
@@ -32,6 +41,15 @@ app.post("/test", (req, res) =>
 
     // und können dies mit res.send(); natürlich auch als eine antwort senden:
     res.send(req.query);
+});
+
+// um eine selbstgemachte 404 fehlerseite anzuzeigen, übergeben wir als letzten pfad ein anonymes callback an app.use(); als middleware und geben dort den statuscode und unseren gewünschten inhalt an:
+app.use((req, res, next) =>
+{
+    // next können wir verwenden um nach dem aufruf von middleware unsere request und response objekte weiterzugeben.
+
+    // wir übergeben den status 404 und senden eine nachricht:
+    res.status(404).send("ERROR: nicht gefunden!");
 });
 
 // auch hier rufen wir eine methode namens .listen(); auf, um dem server zu sagen er soll zuhören:
