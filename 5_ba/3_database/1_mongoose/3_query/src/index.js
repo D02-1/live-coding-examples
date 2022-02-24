215ccc79ab375f49ac85692bf3afc0fe0d94927a
FRickReich <frickreich@gmail.com>
2022-02-24 11:35:06 +0100

    Add advanced query methods

--- 5_ba/3_database/1_mongoose/3_query/src/index.js
+++ 5_ba/3_database/1_mongoose/3_query/src/index.js
@@ -18,6 +18,8 @@ app.use(bodyParser.json());
 
 // Wenn wir dokumente in unserer datenbank finden wollen, haben wir dafür auf modell ebene einige möglichkeiten, die sogenannten "Filter methoden".
 
+// >>>>> READ
+
 // bei einer suche nutzen wir die HTTP methode GET
 
 // ALLE EINTRÄGE FINDEN:
@@ -139,6 +141,170 @@ app.get('/users/direkteId', (req, res) =>
         1  2  3  4  5  6  7  8  9  10 11 12
         ff 14 46 00 b9 d1 68 99 74 93 e1 ee
     */
+});
+
+// ERWEITERTE QUERY METHODEN:
+
+// abgesehen von den typischen filtern, gibt es auch noch ein paar weitere methoden, mit denen wir direkt befehle ausführen können, wenn wir das gesuchte dokument gefunden haben.
+
+// >>>>> UPDATE
+
+// EIN DOKUMENT SUCHEN UND ÄNDERN:
+app.put('/users/updateOne', (req, res) =>
+{
+    // um werte innerhalb eines dokumentes zu ersetzen oder zu ändern, können wir die HTTP methode PUT nutzen, da wir änderungen, also updates, machen.
+    
+    // wir finden mit ...One(); befehlen immer den ersten gefundenen inhalt, wenn es also mehrere dokumente mit diesem wert gibt, wurde nur das erste geändert werden, das gefunden wird.
+
+    // mit Model.findOneAndUpdate(); können wir ein spezifisches dokument suchen, und dort änderungen vornehmen
+    
+    // .findOneAndUpdate({ suchanfrage }, { änderungen }, { optionen });
+
+    User.findOneAndUpdate({ username: "Joe" },
+    // In einem neuen objekt, nach der suchanfrage, geben wir unsere daten an, und nur diese werden überschrieben:
+    {
+        age: 35
+    },
+    // in einem weiteren objekt können wir optionen angeben:
+    {
+        new: true, // wenn wir diese option nicht setzen, bekommen wir im callback das unveränderte dokument zurück, mit dieser option können wir dem benutzer also das veränderte dokument oder die veränderten daten übergeben.
+        upsert: true, // sollte das gesuchte dokument nicht existieren, wird es mit dem upsert befehl erstellt, upsert bedeutet "erstellen bei update"
+        overwrite: false // sollten wir den overwrite befehl auf true setzen, werden alle daten aus dem dokument überschrieben, und nur die im update objekt angegebenen werte übernommen
+    }
+    )
+    .then(user =>
+    {
+        res.status(200).json({
+            success: true,
+            // wir erstellen einen key, in dem wir ausgeben, das daten geändert wurden:
+            updated: user !== null ? true : false,
+            data: user 
+        });
+    });
+});
+
+// EIN DOKUMENT AUF BASIS DER ID SUCHEN UND ÄNDERN
+app.put('/users/updateById', (req, res) =>
+{
+    // Mit Model.findByIdAndUpdate(); suchen wir ein dokument anhand seiner ID und updaten dann die daten, hier gelten die selben optionen wie bei .findOneAndUpdate();
+    User.findByIdAndUpdate("62174600b9d168997493e1f0",
+    {
+        role: "admin"
+    },
+    {
+        new: true
+    })
+    .then(user =>
+    {
+        res.status(200).json({
+            success: true,
+            updated: user !== null ? true : false,
+            data: user
+        });
+    });
+});
+
+// MEHRERE DOKUMENTE VERÄNDERN
+app.put('/users/updateMany', (req, res) =>
+{
+    // um mehrere dokumente auf einmal zu ändern können wir die methode Model.updateMany(); nutzen:
+    User.updateMany({ role: "admin" },
+    {
+        age: 14
+    },
+    {
+        new: true
+    })
+    .then(data =>
+    {
+        res.status(200).json({
+            success: true,
+            data
+        });
+    });
+});
+
+// EIN DOKUMENT ERSETZEN:
+app.put('/users/replace', (req, res) =>
+{
+    // um ein komplettes dokument zu ersetzen, nicht nur die werte auszutauschen, können wir die methode Model.findOneAndReplace(); nutzen, hier gibt es kein FindById für, nur "One", da wir die ID des dokumentes ersetzen, und dies zu problemen führen könnte
+    User.findOneAndReplace({ username: "Steve" },
+    {
+        role: "admin",
+        username: "Mahmut",
+        age: 50,
+        email: "hallo@welt.de",
+        password: "test",
+        country: "mars",
+        job: "Web-Dev"
+    },
+    {
+        new: true
+    })
+    .then(user =>
+    {
+        res.status(200).json({
+            success: true,
+            replaced: user !== null ? true : false,
+            data: user
+        });
+    });
+
+    // wie wir sehen wird auch hier der komplette inhalt überschrieben, es macht also sinn, das wir tatsächlich ALLE inhalte des dokumentes hier angeben, mit dem wir das gefundene dokument ersetzen wollen.
+});
+
+// >>>>> DELETE
+
+// für löschoptionen nutzen wir die HTTP methode DELETE
+
+// wenn wir dokumente löschen wollen, können wir dafür methoden nutzen, die das dokument suchen, und dann das gefundene löschen. hierbei gibt es zwei verschiedene schreibweisen (DELETE und REMOVE), die beide in mongoose das selbe machen, oberflächlich gesehen für uns...
+// sie unterscheiden sich nur in ihrer nutzung auf shell ebene (also mongosh), sie können in mongoose aber absolut austauschbar benutzt werden.
+
+// EIN DOKUMENT FINDEN UND LÖSCHEN
+app.delete('/users/deleteOne', (req, res) =>
+{
+    // mit Model.findOneAndDelete(); oder Model.findOneAndRemove(); suchen wir ein dokument,und löschen dieses, dabei können wir uns das gelöschte dokument im callback ausgeben lassen:
+    User.findOneAndDelete({ username: "Ralph" })
+    .then(user =>
+    {
+        res.status(200).json({
+            success: true,
+            // sollte das zu löschende dokument nicht gefunden werden, bekommen wir hier als daten null zurück, dies sollten wir nutzen, und vielleicht eine weitere nachricht in unsere response einbauen:
+            deleted: user !== null ? true : false,
+            data: user
+        });
+    });
+});
+
+// EIN DOKUMENT ANHAND SEINER ID FINDEN UND LÖSCHEN
+app.delete('/users/deleteById', (req, res) =>
+{
+    // in produktion würden wir diese methode MEIST verwenden, und uns die ID aus den params übergeben lassen, also /users/:id
+
+    User.findByIdAndDelete("62174600b9d168997493e1ee")
+    .then(user =>
+    {
+        res.status(200).json({
+            success: true,
+            deleted: user !== null ? true : false,
+            data: user
+        });
+    });
+});
+
+// MEHRERE DOKUMENTE LÖSCHEN
+app.delete('/users/deleteMany', (req, res) =>
+{
+    // Mit Model.deleteMany(); können wir anhand von suchparametern mehrere dokumente löschen:
+    User.deleteMany({ role: "member" })
+    .then(data =>
+    {
+        res.status(200).json({
+            success: true,
+            deleted: data !== null ? true : false,
+            data
+        });
+    });
 });
 
 app.listen(port, () =>
